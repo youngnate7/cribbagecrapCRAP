79201bc0ad327aba7326c8e43a7babc5345d0626
Nathan <nathanyoung@billericak12.com>
2019-11-13 12:27:12 -0500

    coolmanboolman

--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class Die {
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        Die die = new Die();
+        die.roll();
+        System.out.println(die.getNumDots());
+    }
 }
