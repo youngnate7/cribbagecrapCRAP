7326696844841afa07d4396ab1b9c4ef02bbc9f6
Nathan <nathanyoung@billericak12.com>
2019-11-13 12:11:31 -0500

    coolmanboolman

--- src/com/company/CrapsGame.java
+++ src/com/company/CrapsGame.java
@@ -15,25 +15,39 @@ public class CrapsGame
     public int processRoll(int total)
     {
 
-    int result;
-    switch (total){
-        case 2:
-        case 3:
-        case 12:
-            result = -1;
-            point = 0;
-            break;
-        case 7:
-        case 11:
-            result = 1;
-            point = 0;
-            break;
-        default:
-            result = 0;
-    }
-
-        return result;
-    }
+        int result;
+        if (point == 0) {
+            switch (total) {
+                case 2:
+                case 3:
+                case 12:
+                    result = -1;
+                    point = 0;
+                    break;
+                case 7:
+                case 11:
+                    result = 1;
+                    point = 0;
+                    break;
+                default:
+                    result = 0;
+                    point = total;
+            }
+        }
+        else {
+                 if (total == point) {
+                    result = 1;
+                    point = 0;
+                }
+                else if (total == 7) {
+                    result = -1;
+                    point = 0;
+                } else {
+                    result = 0;
+                }
+            }
+                    return result;
+        }
 
     /**
      *  Returns the saved point
