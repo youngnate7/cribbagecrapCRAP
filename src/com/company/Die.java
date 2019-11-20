21460a92d261916eb66da1d39bb7ad1734132133
Nathan <nathanyoung@billericak12.com>
2019-11-20 11:27:52 -0500

    yup

--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -1,10 +1,22 @@
 package com.company;
 
 public class Die {
+    private int x;
     public static void main(String[] args) {
-// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
         Die die = new Die();
         die.roll();
         System.out.println(die.getNumDots());
+    }
+    public Die(){
+        int x=0;
+    }
+
+    public void roll(){
+        x = (int) (Math.random()*6+1);
+    }
+
+    public int getNumDots(){
+        return x;
     }
 }
